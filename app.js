524ea070b17570dba0dc4f813c04a22553c08f95
arifals27 <54830824+arifals27@users.noreply.github.com>
2022-04-29 12:58:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const todo = require("./todo.js");
 const config = require("./config.json");
 const semaphore = require('semaphore');
-const port = process.env.PORT || 3000;
 
 const sem = semaphore(1);
 const yuhu = new todo.Manga();
@@ -25,6 +24,6 @@ yuhu.updateConfig(config);
         sem.take(function(){yuhu.startCron()});
     }
 res.end(JSON.stringify(config));
-}).listen(port, ()=> {
+}).listen(3000, ()=> {
     console.log("server running. Aceess your domain:3001 to start");
 });
