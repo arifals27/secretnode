87c2000ecc50009ebc4206765788d0482f98604d
arifals27 <54830824+arifals27@users.noreply.github.com>
2022-04-29 16:44:11 +0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -3,28 +3,12 @@ const todo = require("./todo.js");
 const config = require("./config.json");
 const semaphore = require('semaphore');
 const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
 const sem = semaphore(1);
 const yuhu = new todo.Manga();
  // yuhu.get();
 // yuhu.startCron();
 
-http.createServer(function(req, res) {
-const url = req.url;
-    res.writeHead(200, { 'Content-Type' : 'application/json'});
-	
-    if(config.grabber.status === "ready"){
-        config.grabber.status = "running";
-        yuhu.updateConfig(config);
-        yuhu.get();
-    } else if(config.grabber.status === "complete"){
-if(url === "/reset"){
-config.cron.status = false;
-yuhu.updateConfig(config);
-}
-        sem.take(function(){yuhu.startCron()});
-    }
-res.end(JSON.stringify(config));
-}).listen(port, ()=> {
-    console.log(`server running. Aceess your domain:{$port} to start`);
-});
+app..listen(port, ()=> console.log(`server running. Aceess your domain:{$port} to start`);
