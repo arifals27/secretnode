a85dbcbb20fe61b6f0102386ace46d5551e658b6
arifals27 <54830824+arifals27@users.noreply.github.com>
2022-04-29 17:00:22 +0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -8,6 +8,22 @@ const app = express();
 
 const sem = semaphore(1);
 const yuhu = new todo.Manga();
- // yuhu.get();
-// yuhu.startCron();
+
+app.get('/', (req, res) => {
+  const url = req.url;
+    res.writeHead(200, { 'Content-Type' : 'application/json'});
+	
+    if(config.grabber.status === "ready"){
+        config.grabber.status = "running";
+        yuhu.updateConfig(config);
+        yuhu.get();
+    } else if(config.grabber.status === "complete"){
+if(url === "/reset"){
+config.cron.status = false;
+yuhu.updateConfig(config);
+}
+        sem.take(function(){yuhu.startCron()});
+    }
+res.end(JSON.stringify(config));
+})
 app.listen(port, ()=> console.log(`server running. Aceess your domain:{$port} to start`));
